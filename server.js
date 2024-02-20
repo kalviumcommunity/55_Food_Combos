83f65fd86be858d851b6159f172ba9ae7a87facb
Aditya Borhade <143924501+Aditya-335@users.noreply.github.com>
2024-02-20 16:05:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PUBLIC_PORT;
+const port = process.env.PUBLIC_PORT || 3000;
 
 app.get('/ping', (req, res) => {
   res.send('Hello world');

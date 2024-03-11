0e9331fba82e71f2bf4bd0acfb6dc3cf4faa54f0
kalvian2 <aditya.kalvium@community>
2024-03-11 15:01:43 +0530

    added delete functionality

--- Client/Worst-Food-Combos/src/Home.jsx
+++ Client/Worst-Food-Combos/src/Home.jsx
@@ -3,21 +3,37 @@ import './Home.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+const API_URI = `https://server-folder-ftte.onrender.com/read`;
+
 function Home() {
   const [use, setUse] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get("https://server-folder-ftte.onrender.com/read");
+        const res = await axios.get(API_URI);
         setUse(res.data);
       } catch (err) {
         console.log(err);
+        setError("Error fetching data. Please try again.");
       }
     };
 
     fetchData();
   }, []); 
+
+  const deleteItem = async (id) => {
+    try {
+      await axios.delete(`https://server-folder-ftte.onrender.com/read/delete/${id}`);
+      
+      // Update the state after deletion
+      setUse(prevState => prevState.filter(item => item._id !== id));
+    } catch (error) {
+      console.error("Error deleting data:", error);
+      setError("Error deleting data. Please try again.");
+    }
+  };
 
   return (
     <>
@@ -35,15 +51,16 @@ function Home() {
         </div>
         <div className='form'>
           <Link to="/form">
-          <button className='f-btn'>Add Entity</button>
-          
+            <button className='f-btn'>Add Entity</button>
           </Link>
         </div>
       </div>
 
+      {error && <p className="error-message">{error}</p>}
+
       <div className="container">
         {use.map(food => (
-          <div className="food-item" key={food.Img}>
+          <div className="food-item" key={food._id}>
             <div className="card">
               <div className="image">
                 <img src={food.Img} alt="" />
@@ -53,6 +70,11 @@ function Home() {
                 <p className='in'>Veg/Non-Veg: {food.VegOrNonVEG}</p>
                 <p className='in'>Dairy Free: {food.Dairyfree ? "TRUE" : "FALSE"}</p>
                 <p className='in'>Rating: {food.Rating}</p>
+                <div className="btns">
+                  {/* Pass food._id as id to deleteItem */}
+                  <button className='update'>Update</button>
+                  <button className='delete' onClick={() => deleteItem(food._id)}>Delete</button>
+                </div>
               </div>
             </div>
           </div>
